d455d51f1015c33271a6e2322f747fb5ad2bcbb9
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-11-05 19:04:32 +0100

    Añadidos ejercicios de imput-output. Avanza #4

--- imputoutput.R
+++ imputoutput.R
@@ -31,8 +31,31 @@ write.table(matrix(numeros*3, nrow=11, ncol=1), "./datasets/cinco.txt", row.name
 # columna, tengo el contenido de cada fichero.
 #*******************************************************************************************
 
-dos<-read.table("./datasets/dos.txt", header=FALSE)
-tres<-read.table("./datasets/tres.txt", header=FALSE)
-cinco<-read.table("./datasets/cinco.txt", header=FALSE)
+dos<-scan("./datasets/dos.txt")
+tres<-scan("./datasets/tres.txt")
+cinco<-scan("./datasets/cinco.txt")
+
+A<-cbind(dos,tres,cinco)
+
+#*******************************************************************************************
+# Escribe las cinco primera filas de matriz del ejercicio anterior en un fichero nuevo llamado prime.txt
+# y las cinco últimas en otro fichero llamado fin.txt. Ambos ficheros deben tener los datos separados 
+# por comas.
+#*******************************************************************************************
+
+write.table(A[1:5,], "./datasets/prime.txt", row.names=FALSE, col.names=FALSE, sep = ",")
+write.table(A[6:10,], "./datasets/fin.txt", row.names=FALSE, col.names=FALSE, sep = ",")
+
+#*******************************************************************************************
+# Dados dos números introducidos por el usuario f y c, crea un cuadrado de f filas y c columnas con 
+# el caracter "x". Un ejemplo con f=4 y c=3 sería:
+#*******************************************************************************************
+  
+
+print("Introduce un número para las filas")
+f<-scan("")
+print("Introduce un número para las columnas ")
+c <- readline("")  
+matrix("x", nrow = as.numeric(f), ncol = as.numeric(c))
 
 
