c6856850726e868c088998e7a3799cd95e69aa0d
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-11-08 22:29:49 +0100

    Añadidos ejercicios de importar información. Avanza #3.

--- dataframes-listas-tablas.R
+++ dataframes-listas-tablas.R
@@ -12,7 +12,6 @@
 # datos donde ambos Castores están activos.
 #****************************************************************************************
 
-
 x<-cbind(beaver1,"ID"=1)
 
 y<-cbind(beaver2,"ID"=2)
@@ -140,4 +139,119 @@ minorMurder[minorMurder$Murder<4,]
 rownames(USArrests[USArrests[,"UrbanPop"]>quantile(USArrests[,"UrbanPop"],.75),])
 
 
+#****************************************************************************************
+# Crea un dataframe df with 40 columns, as follows: df <- as.data.frame(matrix(sample(1:5, 2000, T), ncol=40))
+#****************************************************************************************
+
+df <- as.data.frame(matrix(sample(1:5, 2000, T), ncol=40))
+
+#Ordena el dataframe por columnas, de izquierda a derecha en orden creciente
+
+#Ordena el dataframe por columnas, de izquierda a derecha en orden decreciente
+
+#Ordena el dataframe por columnas, de derecha a izquierda en orden creciente
+
+#****************************************************************************************
+# Carga el set de datos CO2 y realiza las siguientes acciones:
+#  a. Ordena alfabéticamente los datos en function de la variable Plant. Recuerda que Plant 
+#     es un factor. Imprime el resultado por pantalla para comprobarlo 
+#  b. Ordena los datos en función del incremento de la variable uptake y el orden alfabético 
+#      de la planta (en ese orden)
+#  c. Ordena de nuevo los datos en function del increment de la variable uptake y el orden 
+#      alfabético reverso de la planta (en ese orden)
+#****************************************************************************************
+
+
+
+
+
+
+
+
+#****************************************************************************************
+#****************************************************************************************
+# 3.2 Importando información
+#****************************************************************************************
+#****************************************************************************************
+
+
+
+
+#****************************************************************************************
+# Vamos a trabajar con otro dataframe. Descarga el fichero student.txt de la plataforma PRADO,
+# almacena la información en una variable llamada “students”. Ten en cuenta que los datos son
+# tab-delimited y tienen un texto para cada columna. Comprueba que R ha leído correctamente el
+# fichero imprimiendo el objeto en la pantalla
+#****************************************************************************************
+
+students<-read.table("./datasets/student.txt", header = TRUE)
+students
+
+#****************************************************************************************
+# Imprime solo los nombres de la columnas
+#****************************************************************************************
+
+colnames(students)
+
+#****************************************************************************************
+#Llama a la columna height solo
+#****************************************************************************************
+
+students$height
+
+#****************************************************************************************
+# ¿Cuantas observaciones hay en cada grupo?. Utiliza la función table().  Este commando se puede
+# utilizar para crear tablas cruzadas (cross-tabulations)
+#****************************************************************************************
+
+table(students)
+
+#****************************************************************************************
+# Crea nuevas variables a partir de los datos que tenemos. Vamos a crear una variable nueva
+# “sym” que contenga M si el genero es masculino y F si el genero es femenino. Busca en la 
+# ayuda información sobre la función ifelse(). Crea una segunda variable “colours” cuyo valor
+# será “Blue” si el estudiante es de kuopio y “Red” si es de otro sitio.
+#****************************************************************************************
+
+sym<-ifelse(students$gender=="male", "M", "F")
+colours<-ifelse(students$population=="kuopio", "Blue", "Red")
+
+#****************************************************************************************
+#Con los datos anteriores de height y shoesize y las nuevas variables crea un nuevo data.frame 
+#que se llame students.new
+#****************************************************************************************
+
+students.new<-cbind(students,sym, colours)
+
+#****************************************************************************************
+#Comprueba que la clase de student.new es un dataframe
+#****************************************************************************************
+
+str(students.new)
+
+#****************************************************************************************
+# Crea dos subsets a partir del dataset student. Dividelo dependiendo del sexo. Para ello 
+# primero comprueba que estudiantes son hombres (male). Pista: busca información sobre la función which.
+#****************************************************************************************
+
+
+#****************************************************************************************
+# Basándote en esa selección dada por which() toma solo esas filas del dataset student para generar
+# el subset stundent.male
+#****************************************************************************************
+
+students.male = students.new[which(students.new$sym=="M"),]
+
+#****************************************************************************************
+#Repite el procedimiento para seleccionar las estudiantes mujeres (females)
+#****************************************************************************************
+
+students.female = students.new[which(students.new$sym=="F"),]
+
+
+#****************************************************************************************
+#Utiliza la function write.table() para guarder el contenido de student.new en un archivo.
+#****************************************************************************************
+
+write.table(students.new, "./datasets/studentsnew.txt", col.names = TRUE)
 
